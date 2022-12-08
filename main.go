fd5d0b606c3ffb27586b3112ddf300f8b9bf4e38
kinosuke01 <kinosuke01@pepabo.com>
2022-12-08 22:27:52 +0900

    add logInfo

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./"))
-	mux.Handle("/", http.StripPrefix("/", fileServer))
+	mux.Handle("/", http.StripPrefix("/", logInfo(fileServer)))
 	server := http.Server{
 		Addr:    ":80",
 		Handler: mux,
 	}
 	server.ListenAndServe()
+}
+
+func logInfo(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rAddr := r.RemoteAddr
+		method := r.Method
+		path := r.URL.Path
+		fmt.Printf("Remote: %s [%s] %s\n", rAddr, method, path)
+		h.ServeHTTP(w, r)
+	})
 }
